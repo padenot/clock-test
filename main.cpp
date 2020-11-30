cfb6969ddd0fa695d7c9684633d7b5b22875f708
Paul Adenot <paul@paul.cx>
2020-11-30 17:07:26 +0100

    macOS

--- main.cpp
+++ main.cpp
@@ -1,10 +1,28 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <inttypes.h>
 
 enum ClockType {
   INCREMENT_SLEEP, // the clock increments while the computer is asleep/suspended
   INCREMENT_AWAKE // the clock increments while the computer is awake
 };
+
+// Apple things
+#if defined(__APPLE__) && defined(__MACH__)
+#include <time.h>
+
+uint64_t get_time_ms(ClockType type) {
+  uint64_t rv = 0;
+  if (type == INCREMENT_SLEEP) {
+    // https://developer.apple.com/documentation/kernel/1462446-mach_absolute_time recommends this
+    rv = clock_gettime_nsec_np(CLOCK_MONOTONIC_RAW) / 1e6;
+  } else if (type == INCREMENT_AWAKE) {
+    // https://developer.apple.com/documentation/kernel/1646199-mach_continuous_time recommends this
+    rv = clock_gettime_nsec_np(CLOCK_UPTIME_RAW) / 1e6;
+  }
+  return rv;
+}
+#endif
 
 // Linux desktop + android
 #ifdef __linux__
@@ -32,15 +50,16 @@ uint64_t get_time_ms(ClockType type) {
 
 int main()
 {
-  int program_start_no_sleep = get_time_ms(INCREMENT_AWAKE);
-  int program_start_sleep = get_time_ms(INCREMENT_SLEEP);
-  printf("Time is %dms (awake time %dms)\n", program_start_sleep, program_start_no_sleep);
+  uint64_t program_start_no_sleep = get_time_ms(INCREMENT_AWAKE);
+  uint64_t program_start_sleep = get_time_ms(INCREMENT_SLEEP);
+  printf("Time is %" PRIu64 "ms (awake time %" PRIu64 "ms)\n", program_start_sleep, program_start_no_sleep);
   printf("Put the computer to sleep, come back in some time..., and type 'y'");
   while(getchar() != 'y') {
     printf("Didn't understand sorry.\n");
   }
-  int program_end_no_sleep = get_time_ms(INCREMENT_AWAKE);
-  int program_end_sleep = get_time_ms(INCREMENT_SLEEP);
-  printf("Time is %dms (awake time %dms)\n", program_end_sleep, program_end_no_sleep);
-  printf("Program duration was %dms (awake time %dms)\n", program_end_sleep - program_start_sleep, program_end_no_sleep - program_start_no_sleep);
+  uint64_t program_end_no_sleep = get_time_ms(INCREMENT_AWAKE);
+  uint64_t program_end_sleep = get_time_ms(INCREMENT_SLEEP);
+  printf("Time is %" PRIu64 "ms (awake time %" PRIu64 "ms)\n", program_end_sleep, program_end_no_sleep);
+  printf("Program duration was %" PRIu64 "ms (awake time %" PRIu64 "ms)\n", program_end_sleep - program_start_sleep, program_end_no_sleep - program_start_no_sleep);
+  return 0;
 }
